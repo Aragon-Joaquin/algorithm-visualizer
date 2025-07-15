5b7b046ac514ac7432d536553060cdfc5cb72265
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-07-15 20:26:30 -0300

    :star: testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,65 @@
 export default function App() {
 	return <></>
 }
+
+interface Maze {
+	XSquares: number
+	YSquares: number
+}
+
+interface Square {
+	width: number
+	height: number
+	edge?: {
+		top: boolean
+		right: boolean
+		bottom: boolean
+		left: boolean
+	}
+}
+
+function getCanvas() {
+	const canvas = document?.getElementById('main-canvas') as HTMLCanvasElement | null
+	const ctx = canvas?.getContext('2d')
+
+	if (!ctx || !canvas) return
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	createMaze(ctx, { XSquares: 25, YSquares: 20 }, { height: canvas.height, width: canvas.width })
+}
+
+function createMaze(ctx: CanvasRenderingContext2D, Maze: Maze, canvas: { height: number; width: number }) {
+	const { XSquares, YSquares } = Maze
+	const { height, width } = canvas
+
+	const SquareProps: Square = {
+		width: width / XSquares,
+		height: height / YSquares
+	}
+
+	// borders
+	ctx.strokeStyle = 'white'
+	for (let i = 0; i < XSquares; i++) {
+		for (let j = 0; j < YSquares; j++) {
+			ctx.strokeRect(i * SquareProps.width, j * SquareProps.height, SquareProps.width, SquareProps.height)
+		}
+	}
+
+	// squares
+	ctx.strokeStyle = 'black'
+	ctx.fillStyle = 'white'
+	for (let i = 0; i < XSquares; i++) {
+		for (let j = 0; j < YSquares; j++) {
+			ctx.fillRect(i * SquareProps.width + 1, j * SquareProps.height + 1, SquareProps.width - 2, SquareProps.height - 2)
+		}
+	}
+}
+
+getCanvas()
+
+//! Kruskal's Algorithm (for now)
+function algorithmMaze(xAxis: number, yAxis: number): Square[] {
+	let maze: Square[] = []
+
+	return maze
+}
