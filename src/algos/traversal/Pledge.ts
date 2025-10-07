a145832eb9f1dfb262ef3e987846b9c7ec8b2e79
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-10-06 23:26:17 -0300

    i was looking the theory of the algo and i skipped some essential rules...

--- src/algos/traversal/Pledge.ts
+++ src/algos/traversal/Pledge.ts
@@ -37,6 +37,7 @@ export function traversalPledge({ EndPoint, Nodes }: TraversalProps) {
 	} as const
 
 	let currentDegrees: keyof typeof positionMap = 0
+	let turnAngle: number = 0
 	let previousMoment: keyof typeof possibleMovements = 'top'
 
 	return function* recursive(node: mazeCoords): GenReturn {
@@ -47,7 +48,13 @@ export function traversalPledge({ EndPoint, Nodes }: TraversalProps) {
 		if (EndPoint.x === x && EndPoint.y === y) yield
 
 		const pos = positionMap[((currentDegrees % 360) + 360) % 360] // we position we're heading to, e.g. "right"
-		const calculateMovement = (assertedEdge: string) => (possibleMovements[pos].at(0) === assertedEdge ? -90 : 90)
+		const calculateMovement = (assertedEdge: keyof Square['edge'], position: keyof Square['edge']) => {
+			const val = possibleMovements[pos].at(0) === assertedEdge ? -90 : 90
+
+			previousMoment = position
+			if (Math.abs(val + currentDegrees) === 360) currentDegrees = 0
+			else currentDegrees += val
+		}
 
 		//reduce iterations. e.g. when iterating over right, its only possible ["bottom","top"]
 		for (const key of [previousMoment, ...possibleMovements[pos]]) {
@@ -55,6 +62,16 @@ export function traversalPledge({ EndPoint, Nodes }: TraversalProps) {
 			const adjNode = getAdjacentNode(Nodes, y, x)[pos]()! // its never null if we checked the edges
 
 			console.log(node, edge, { pos, currentDegrees, assertedEdge })
+
+			//check if top.
+			if (pos === 'top' && edge[pos]) {
+				console.log('top case', { previousMoment })
+				const calcMov = previousMoment === 'left' ? 'left' : 'right'
+				const moveEpic = getAdjacentNode(Nodes, y, x)[calcMov]()
+				if (!moveEpic) continue
+				calculateMovement(calcMov, pos)
+				yield* recursive({ x: moveEpic.x, y: moveEpic.y })
+			}
 
 			//can go straight if there's no wall in front && the next node has a wall to its left: ______
 			if (!edge[pos] && adjNode.edge[assertedEdge]) {
@@ -68,10 +85,9 @@ export function traversalPledge({ EndPoint, Nodes }: TraversalProps) {
 			if (edge[pos]) {
 				const nextNode = getAdjacentNode(Nodes, y, x)[assertedEdge]()
 				console.log('hitted wall:', { nextNode })
-				if (!nextNode) continue
-				currentDegrees += calculateMovement(assertedEdge)
-				previousMoment = pos
-				yield* recursive({ x: node.x, y: node.y })
+				if (!nextNode || previousMoment === assertedEdge) continue
+				calculateMovement(assertedEdge, pos)
+				yield* recursive({ x, y })
 				break
 			}
 
@@ -80,17 +96,17 @@ export function traversalPledge({ EndPoint, Nodes }: TraversalProps) {
 			console.log('hitted corner: ', { moveTo })
 			if (!moveTo) continue
 
-			if (moveTo.edge[leftToMove[pos]]) {
-				currentDegrees += calculateMovement(assertedEdge)
-				previousMoment = pos
+			if (moveTo.edge[OPPOSING_EDGES[pos]]) {
+				console.log('NON 360', moveTo)
+				calculateMovement(assertedEdge, pos)
 				yield* recursive({ x: moveTo.x, y: moveTo.y })
 			} else {
+				console.log('did 360')
 				//we need to do a 360 in this case
 				if (moveTo.edge[OPPOSING_EDGES[pos]]) continue
 				const nextNextNode = getAdjacentNode(Nodes, moveTo.y, moveTo.x)[OPPOSING_EDGES[pos]]()
 				if (!nextNextNode) continue
-				currentDegrees += calculateMovement(assertedEdge)
-				previousMoment = leftToMove[pos]
+				calculateMovement(OPPOSING_EDGES[pos], OPPOSING_EDGES[pos])
 				yield* recursive({ x: nextNextNode.x, y: nextNextNode.y })
 			}
 			continue
